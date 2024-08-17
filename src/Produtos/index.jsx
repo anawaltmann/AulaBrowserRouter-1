4e040503620fdbe6b876ecf46291f23211f5b5e4
anawaltmann <161073911+anawaltmann@users.noreply.github.com>
2024-08-16 22:57:16 -0400

    c

--- src/Produtos/index.jsx
+++ src/Produtos/index.jsx
@@ -2,15 +2,51 @@ import { useState } from "react"
 import Header from "../components/Header";
 import CardProdutos from "../components/CardProdutos";
 
-export default function Produto(){
+export default function Produto() {
 
-    const[lista,setLista] = useState([{ title:"Anel Vivara em Ouro Amarelo 18k com Diamantes", preco: "R$ 3.720,00" },{  title:"Anel Vivara em Ouro Branco 18k com Diamantes", preco: "R$ 3.512,00" }]);
-    return(
+    const [lista, setLista] = useState([
+        { 
+            title: "Anel Vivara em Ouro Amarelo 18k com Diamantes", 
+            preco: "R$ 3.720,00", 
+            imagem: "https://lojavivara.vtexassets.com/arquivos/ids/745172/Anel-Vivara-em-Ouro-Amarelo-18k-com-Diamantes-8689_1_set.jpg?v=638437289332230000" 
+        },
+        { 
+            title: "Anel Vivara em Ouro Branco 18k com Diamantes", 
+            preco: "R$ 3.512,00", 
+            imagem: "https://lojavivara.vtexassets.com/arquivos/ids/745171/Anel-Vivara-em-Ouro-Branco-18k-com-Diamantes-8704_1_set.jpg?v=638437289331930000" 
+        },
+        { 
+            title: "Brinco Vivara em Ouro Rosé 18ks", 
+            preco: "R$ 3.290,00", 
+            imagem: "https://lojavivara.vtexassets.com/unsafe/480x480/center/middle/https%3A%2F%2Flojavivara.vtexassets.com%2Farquivos%2Fids%2F709507%2FBrinco-Vivara-em-Ouro-Rose-18k-9220_hover.jpg%3Fv%3D638388091925700000" 
+        },
+        { 
+            title: "Brinco Vivara em Ouro Vivara 18k com Diamantes Negros", 
+            preco: "R$ 2.520,00", 
+            imagem: "https://lojavivara.vtexassets.com/unsafe/480x480/center/middle/https%3A%2F%2Flojavivara.vtexassets.com%2Farquivos%2Fids%2F851692%2FBrinco-Vivara-em-Ouro-Vivara-18k-com-Diamantes-Negros-28148_hover.jpg%3Fv%3D638484747690030000" 
+        },
+        { 
+            title: "Anel Vivara em Ouro Branco 18k com Diamantes", 
+            preco: "R$ 3.512,00", 
+            imagem: "https://lojavivara.vtexassets.com/arquivos/ids/745171/Anel-Vivara-em-Ouro-Branco-18k-com-Diamantes-8704_1_set.jpg?v=638437289331930000" 
+        },
+        { 
+            title: "Anel Vivara em Ouro Branco 18k com Diamantes", 
+            preco: "R$ 3.512,00", 
+            imagem: "https://lojavivara.vtexassets.com/arquivos/ids/745171/Anel-Vivara-em-Ouro-Branco-18k-com-Diamantes-8704_1_set.jpg?v=638437289331930000" 
+        },
+        { 
+            title: "Anel Vivara em Ouro Branco 18k com Diamantes", 
+            preco: "R$ 3.512,00", 
+            imagem: "https://lojavivara.vtexassets.com/arquivos/ids/745171/Anel-Vivara-em-Ouro-Branco-18k-com-Diamantes-8704_1_set.jpg?v=638437289331930000" 
+        }
+    ]);
+
+    return (
         <div>
-            <Header/>
+            <Header />
             <h1>Produtos</h1>
-            <CardProdutos listaProdutos={lista}/>
+            <CardProdutos listaProdutos={lista} />
         </div>
-      
     );
 }
--- src/components/CardProdutos.jsx
+++ src/components/CardProdutos.jsx
@@ -1,13 +1,19 @@
-export default function CardProdutos({listaProdutos}){
-    return(
+export default function CardProdutos({ listaProdutos }) {
+    return (
         <div>
-            { listaProdutos.map( (produto) => 
-                <div key={produto.title}>
+            {listaProdutos.map((produto) => (
+                <div key={produto.title} style={{ border: "1px solid #ccc", margin: "10px", padding: "10px" }}>
+                    {/* Exibindo a imagem */}
+                    <img 
+                        src={produto.imagem} 
+                        alt={produto.title} 
+                        style={{ width: "100px", height: "100px" }} 
+                    />
+                    {/* Exibindo o título e preço */}
                     <p>{produto.title}</p>
                     <p>{produto.preco}</p>
                 </div>
-            
-            )}
+            ))}
         </div>
     );
 }
